8e9561fb3ba9c9cf8ff6ac0be725783c24b6ce41
ms@wesewise.com <ms@wesewise.com>
2021-03-31 13:24:38 +0500

    add and use axios

---
+++ src/httpCommon.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+import axios from "axios"
+
+export default axios({
+  baseURL: "http://api.custimoo.com/api",
+  headers: {
+    "Content-type": "application/json"
+  }
+})
+</script>
---
+++ src/services/ApiDataService.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import http from "../httpCommon.vue"
+
+class ApiDataService {
+  getAll(payload: string) {
+    return http.get(`/list/products${payload}`)
+  }
+  getSingle(payload: string) {
+    return http.get(`/product/colors${payload}`)
+  }
+  // get(id: string) {
+  //   return http.get(`/tutorials/${id}`);
+  // }
+}
+
+export default new ApiDataService()
+</script>
