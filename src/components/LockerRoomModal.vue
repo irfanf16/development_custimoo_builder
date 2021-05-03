04aa71eeec744f0ec49c4617db5ea4ad28325df5
muhammadaamir1994 <ar@weiswise.com>
2021-05-03 15:56:17 +0500

    Changes

--- src/components/LockerRoomModal.vue
+++ src/components/LockerRoomModal.vue
@@ -107,122 +107,117 @@
 </script>
 
 <style lang="scss" scoped>
-    .lockerroom-header{
-        display: flex;
-        flex-wrap: wrap;
-        align-items: center;
-        justify-content: space-between;
-        .locker-opener{
-            max-width: 90%;
-            padding: 15px;
-            font-size: 18px;
-            position: relative;
-            overflow-x: auto;
-            white-space: nowrap;
-            @media only screen and (min-width: 992px){
-                max-width: 100%;
-                padding: 14px 30px;
-                max-width: 80%;
-            }
-            .btn{
-                padding: 5px 10px;
-                margin: 0 5px 10px;
-                position: relative;
-                background: none;
-                border-color: rgba(3,20,46,0.13);
-                color: #03142E;
-                font-size: 0.8rem;
-                @media only screen and (min-width: 992px){
-                    padding: 10px 30px;
-                    margin: 0 10px 10px;
-                    font-size: 1rem;
-                }
-                &.active,
-                &:hover{
-                    background: #219f84;
-                    color: #fff;
-                    border-color: #219f84;
-                }
-                .remove{
-                    position: absolute;
-                    right: -10px;
-                    top: -14px;
-                    width: 20px;
-                    height: 20px;
-                    font-size: 9px;
-                    color: #D53943;
-                    background: #F8E1E2;
-                    border-radius: 50%;
-                    display: flex;
-                    flex-wrap: wrap;
-                    align-items: center;
-                    justify-content: center;
-                    @media only screen and (min-width: 992px){
-                        width: 30px;
-                        height: 30px;
-                        font-size: 12px;
-                    }
-                }
-            }
-            .arrow{
-                position: absolute;
-                left: 0;
-                top: 50%;
-                transform: translateY(-50%);
-                z-index: 1;
-                color: #219f84;
-                font-size: 15px;
-                display: none;
-                @media only screen and (min-width: 992px){
-                    display: inline-block;
-                }
-                &.arrow-right{
-                    left: auto;
-                    right: 0;
-                }
-            }
+.lockerroom-header{
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: space-between;
+    .locker-opener{
+        max-width: 90%;
+        padding: 15px;
+        font-size: 18px;
+        position: relative;
+        overflow-x: auto;
+        white-space: nowrap;
+        @media only screen and (min-width: 992px){
+            max-width: 100%;
+            padding: 14px 30px;
+            max-width: 80%;
         }
-        
-    }
-    .create-lockerroom{
-            .btn{
-                padding: 0;
-                font-size: 24px;
-                line-height: 1;
-                font-weight: 700px;
-                color: #fff;
+        .btn{
+            padding: 5px 10px;
+            margin: 0 5px 10px;
+            position: relative;
+            background: none;
+            border-color: rgba(3,20,46,0.13);
+            color: #03142E;
+            font-size: 0.8rem;
+            @media only screen and (min-width: 992px){
+                padding: 10px 30px;
+                margin: 0 10px 10px;
+                font-size: 1rem;
+            }
+            &.active,
+            &:hover{
                 background: #219f84;
-                width: 24px;
-                height: 24px;
+                color: #fff;
+                border-color: #219f84;
+            }
+            .remove{
+                position: absolute;
+                right: -10px;
+                top: -14px;
+                width: 20px;
+                height: 20px;
+                font-size: 9px;
+                color: #D53943;
+                background: #F8E1E2;
                 border-radius: 50%;
                 display: flex;
                 flex-wrap: wrap;
-                justify-content: center;
                 align-items: center;
-                border: none;
+                justify-content: center;
                 @media only screen and (min-width: 992px){
-                    padding: 10px 30px;
-                    color: #219f84;
-                    background: #E7F4F1;
-                    border: 1px solid #E7F4F1;
-                    border-radius: 0.25rem;
-                    width: auto;
-                    height: auto;
-                    font-size: 14px;
-                    font-weight: 400;
-                }
-                span{
-                    @media only screen and (max-width: 991px){display: none;}
+                    width: 30px;
+                    height: 30px;
+                    font-size: 12px;
                 }
             }
         }
-<<<<<<< HEAD
-    
-    
-</style>
-=======
+        .arrow{
+            position: absolute;
+            left: 0;
+            top: 50%;
+            transform: translateY(-50%);
+            z-index: 1;
+            color: #219f84;
+            font-size: 15px;
+            display: none;
+            @media only screen and (min-width: 992px){
+                display: inline-block;
+            }
+            &.arrow-right{
+                left: auto;
+                right: 0;
+            }
+        }
     }
+    
+}
+.create-lockerroom{
+    .btn{
+        padding: 0;
+        font-size: 24px;
+        line-height: 1;
+        font-weight: 700px;
+        color: #fff;
+        background: #219f84;
+        width: 24px;
+        height: 24px;
+        border-radius: 50%;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        align-items: center;
+        border: none;
+        @media only screen and (min-width: 992px){
+            padding: 10px 30px;
+            color: #219f84;
+            background: #E7F4F1;
+            border: 1px solid #E7F4F1;
+            border-radius: 0.25rem;
+            width: auto;
+            height: auto;
+            font-size: 14px;
+            font-weight: 400;
+        }
+        span{
+            @media only screen and (max-width: 991px){display: none;}
+        }
+    }
+}
+    
+    
 
 
 </style>
->>>>>>> cee31bf28efa039e9313eb1b5a2dd08744507898
