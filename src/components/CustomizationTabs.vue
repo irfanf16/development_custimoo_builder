92b9ab736e702cceaceadf32b6864df5d11e1b38
yasir <yr@weiswise.com>
2021-05-11 14:16:25 +0500

    Back and next error fixed

--- src/components/CustomizationTabs.vue
+++ src/components/CustomizationTabs.vue
@@ -178,16 +178,17 @@ export default class CustomizationProcess extends Vue {
 
   public productColorsManipulation() {
     this.selectedProduct.colors.forEach((colors: any, key: number) => {
-      colors.color_text = JSON.parse(colors.color_text)
-      colors.selectedColor = ""
-      this.productColors = this.productColors.concat(colors)
+      let finalColor = {color_text: [], selectedColor: ""}
+      finalColor.color_text = JSON.parse(colors.color_text)
+      this.productColors = this.productColors.concat(finalColor)
     })
   }
 
   public fontsColorsManipulation() {
     this.selectedProduct.namecolors.forEach((colors: any, key: number) => {
-      colors.color_text = JSON.parse(colors.color_text)
-      this.fontsColors = this.fontsColors.concat(colors)
+      let finalColor = {color_text: []}
+      finalColor.color_text = JSON.parse(colors.color_text)
+      this.fontsColors = this.fontsColors.concat(finalColor)
     })
     if (this.fontsColors.length) {
       this.firstColor = this.fontsColors[0].color_text[0].value
