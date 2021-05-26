29d06e6f38a89d6a9db656191321225f4568c356
awais <bcs.12.43@gmail.com>
2021-05-26 11:49:08 +0500

    adding the download roster function

--- src/components/EditRosterArea.vue
+++ src/components/EditRosterArea.vue
@@ -38,21 +38,7 @@
         </div>
       </div>
     </b-modal>
-    <b-modal ref="myModal" content-class="upload-logo-disclaimer roster-msg" id="modal-center-uploadroster" centered scrollable size="lg" title="Upload Team Roster">
-        <p class="mb-4">The Team Roster can be automatically imported from an excel sheet. Please download and use the excel sheet below. No other excel sheets or documents can be used to import data.</p>
-        <div class="roster-template-area">
-            <b-button class="btn btn-secondary fw-bold">Download Roster Template <a href="#" v-b-tooltip.hover
-                                                                                  title="Enter roster in excel file">
-              <font-awesome-icon :icon="['fas', 'info-circle']"/>
-            </a></b-button>
 
-            <b-button type="upload" name="Upload Template" @change="onChange" class="btn btn-secondary fw-bold" accept="image/x-png,image/jpeg,pdf">Upload Roster Template
-              <b-form-file  class="mb-2"></b-form-file>
-              <a href="#" v-b-tooltip.hover title="Upload the template here to populate the roster">
-              <font-awesome-icon :icon="['fas', 'info-circle']"/>
-            </a></b-button>
-        </div>
-      </b-modal>
     <div class="d-lg-none">
       <RosterDetails :productSizes="productSizes"/>
     </div>
@@ -145,129 +131,10 @@ export default class EditRosterArea extends Vue {
     let count = (val.match(/\*/g) || []).length;
     return count
   }
-  public onChange(event: Record<any, any>){
-    let status = true;
-    let loopStatus = true;
-    let files = event.target.files ? event.target.files[0] : null;
-    readXlsxFile(files).then((rows: any[][]) => {
-      if (rows[0].length != 6){
-        alert("please upload valid file")
-        return false
-      }
-      for (let i in rows[0]){
-        if (i == '1'){
-          let count = this.getOccurence(rows[0][i]);
-          if (count != 1 || rows[0][i] != "SIZE*"){
-            status = false
-            break;
-          }
-        }
-        if (i == '2'){
-          let count = this.getOccurence(rows[0][i]);
-          if (count != 2 || rows[0][i] != "NAME ON PRODUCT**"){
-            status = false
-            break;
-          }
-        }
-        if (i == '4'){
-          let count = this.getOccurence(rows[0][i]);
-          if (count != 3 || rows[0][i] != "OTHER INFORMATION***"){
-            status = false
-            break;
-          }
-        }
-      }
-      if (status) {
-        for (let row in rows){
-          let obj = {
-            text: '',
-            number: '',
-            size: '',
-            quantity: 1,
-            information: ''
-          };
-          for (let i in rows[row]){
-            if (row == '0'){
-              continue
-            }
-            if (i == '1') {
-              if (rows[row][i] == null || !this.checkSize(rows[row][i])){
-                loopStatus = false;
-                break;
-              }
-              obj.size   = rows[row][i];
-            }
-            if (i == '2') {
-              obj.text = rows[row][i];
-            }
-            if (i == '3') {
-              obj.number = rows[row][i];
-            }
-            if (i == '4') {
-              obj.information = rows[row][i];
-            }
-          }
-          if (loopStatus == false){
-            break
-          }
-          this.fileData.push(obj);
-        }
-        if (loopStatus == true){
-          this.$store.dispatch('updateRoster', this.fileData);
-        }else{
-          alert('Size is missing');
-        }
-      }else{
-        alert("please upload a valid file");
-      }
-    })
-  }
-  public checkSize(size:string){
-    let sizes = ['SM', 'MD', 'LG', 'XL','2XL', '3XL'];
-    return sizes.includes(size);
-  }
+
+
 }
 
 </script>
 
-<style lang="scss" scoped>
-  .roster-template-area{
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-between;
-        align-items: center;
-        .btn-secondary{
-            font-size: 14px;
-            color: #219F84;
-            background: #E7F4F1;
-            font-weight: 500;
-            flex: 0 0 48%;
-            max-width: 48%;
-            border-color: #E7F4F1;
-            transition: all 0.3s ease;
-            position: relative;
-            &:hover{
-                background: #219f84;
-                color: #fff;
-                a{color: #fff;}
-            }
-            .custom-file{
-                position: absolute;
-                right: 22%;
-                left: 0;
-                top: 0;
-                bottom: 0;
-                opacity: 0;
-                margin: 0;
-                width: auto;
-            }
-            a{
-                color: #219f84;
-                margin: 0 0 0 3px;
-                &:hover{
-                    color: #fff;
-                }
-            }
-        }
-    }
-</style>
+
