36f3a2f1604acf42e2e4526699a92489db6bac38
muhammadaamir1994 <ar@weiswise.com>
2021-06-02 20:02:27 +0500

    3D View Added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -73,8 +73,17 @@
             </div>
           </template>
           <div class="customization-area d-flex flex-wrap justify-content-center align-items-center" :class="{'mobile-custom-scroll': (hideTab.logoHide || hideTab.colorHide || hideTab.textHide || hideTab.styleHide || hideTab.teamHide) }">
-            <div>
-              <CustomizationPreview />
+            <div v-bind:class="{active: isActive}">
+              <b-button class="preview-btn" variant="secondary" v-on:click="myFilter">
+                <span class="three-d-btn"><font-awesome-icon :icon="['fas', 'cube']"/> 3D View</span>
+                <span class="two-d-btn"><font-awesome-icon :icon="['fas', 'dice-two']"/> 2D View</span>
+              </b-button>
+              <div class="twoD-view">
+                <CustomizationPreview />
+              </div>
+              <div class="threeD-view">
+                <CustomizationPreviewThreeD />
+              </div>
               <template v-if="manageComponents.BasicCustomization">
                 <b-button @click="showAdvanceCustomization()" class="d-none d-lg-inline-block mt-5" variant="secondary">Continue</b-button>
               </template>
@@ -108,6 +117,7 @@ import SaveColorModal from "@/components/SaveColorModal.vue"
 import UploadLogo from '@/components/UploadLogo.vue'
 import LockerRoomModal from '@/components/LockerRoomModal.vue'
 import AddLockerRoomModal from '@/components/AddLockerRoomModal.vue'
+import CustomizationPreviewThreeD from '@/components/CustomizationPreviewThreeD.vue'
 import {http} from "@/httpCommon"
 
 @Component<Home>({
@@ -120,7 +130,8 @@ import {http} from "@/httpCommon"
     UploadLogo,
     LockerRoomModal,
     AddLockerRoomModal,
-    SaveColorModal
+    SaveColorModal,
+    CustomizationPreviewThreeD
   },
   mounted() {
     if (this.isAuthenticated) {
@@ -354,10 +365,17 @@ export default class Home extends Vue {
   public buyNow() {
     this.$router.push('/confirm-order')
   }
+
+  public isActive = false;
+  public myFilter() {
+    this.isActive = !this.isActive
+  }
+
 }
 </script>
 
 <style lang="scss" scoped>
+  
 .page-wrapper {
   @media only screen and (min-width: 992px) {
     border: 1px solid #dee2e6;
@@ -585,6 +603,37 @@ export default class Home extends Vue {
   }
 
 }
+.customization-area{
+  .preview-btn{
+    position: absolute;
+    left: 0;
+    top: 20px;
+    font-size: 0.7rem;
+    @media only screen and (min-width: 768px){
+      left: auto;
+      right: 0;
+      top: -30px;
+      font-size: 1rem;
+    }
+    svg{margin-right: 5px;}
+  }
+  .two-d-btn{
+    display: none;
+  }
+  .threeD-view{display: none;}
+  .active{
+    .threeD-view,
+    .two-d-btn{
+      display: block;
+    }
+    .twoD-view,
+    .three-d-btn{
+      display: none;
+    }
+  }
+  
+}
+
 
 
 </style>
