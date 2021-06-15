646a547449007fea6d8e1e2c7921f384bbc61f88
muhammadaamir1994 <ar@weiswise.com>
2021-06-15 13:18:47 +0500

    Locker room Assets use btn settings

--- src/components/LockerRoomModal.vue
+++ src/components/LockerRoomModal.vue
@@ -59,8 +59,9 @@
                               </b-tab>
                                 <b-tab title="Assets" class="assets-file">
                                   <template v-for="(logo, inda) in room.logos">
-                                    <div :key="inda">
-                                      <img @click="addToCustomLogos(logo)"  :src="apiBaseUrl+'/'+logo.logo_url "/>
+                                    <div :key="inda" class="assets-logo-block">
+                                      <img :src="apiBaseUrl+'/'+logo.logo_url "/>
+                                      <button @click="addToCustomLogos(logo)" class="use-logo-btn">Use</button>
                                     </div>
                                   </template>
                                 </b-tab>
@@ -204,6 +205,8 @@
         }else{
           alert("logo upload limit exceed")
         }
+        this.ref['locker-modal'].hide();
+
       }
 
     }
@@ -564,6 +567,30 @@
             max-height: 20vh;
             @media only screen and (min-width: 768px){max-height: 50vh;}
         }
+    }
+
+    .assets-logo-block{
+      position: relative;
+      &:hover{
+        .use-logo-btn{transform: scale(1);}
+      }
+    }
+
+    .use-logo-btn{
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      background: rgba(0,0,0,0.5);
+      z-index: 1;
+      color: #fff;
+      text-transform: uppercase;
+      font-size: 1.5rem;
+      transition: all 0.3s ease;
+      transform: scale(0);
+      width: 100%;
+      border: none;
     }
 
 
