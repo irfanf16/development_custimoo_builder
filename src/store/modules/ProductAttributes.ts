3ac5ce4461f02a9199b9e36a283f1d1748b03482
muhammadaamir1994 <ar@weiswise.com>
2021-07-16 19:27:54 +0500

    UI Tabs changes

--- src/store/modules/ProductAttributes.ts
+++ src/store/modules/ProductAttributes.ts
@@ -38,6 +38,7 @@ const ProductAttributes:Module<any, any> = {
       }
     },
     customLogos(state: Record<any, any>, customLogo: Record<any, any>) {
+      // Vue.set(state.customLogos, state.customLogos.length, customLogo)
       if(customLogo){
         if('logoIndex' in customLogo && customLogo.logoIndex != null) {
           Vue.set(state.customLogos, customLogo.logoIndex, customLogo)
@@ -314,7 +315,6 @@ const ProductAttributes:Module<any, any> = {
     },
 
     setLogoTab({commit}, payload){
-      console.log('setLogoTab',payload)
       commit('setLogoTabMutation', payload)
     },
     toggleLogoBackgroud({commit}, index){
