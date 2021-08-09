6621e40c64fb9adb17205e3afee844d0e8e323e0
Muhammad Ramzan <ra@wesewise.com>
2021-08-09 17:31:35 +0500

    hide color sections in case of personalized items

--- src/components/LogoPlacementTabs.vue
+++ src/components/LogoPlacementTabs.vue
@@ -34,7 +34,7 @@
               </template>
               <SaveLogoModal :logoIndex="index" />
             </div>
-            <div class="logo-placement-area extracted-color-area" v-if="index == 0">
+            <div class="logo-placement-area extracted-color-area" v-if="index == 0 && !hideColorSection" >
               <h4 class="mb-3 mb-lg-4">Color Extracted from Logo</h4>
               <div class="logo-placement-holder mb-lg-3">
                 <div class="logo-holder color-extracted-area">
@@ -133,6 +133,10 @@ export default class LogoPlacementTabs extends Vue {
   }
   get isCustomerAuthenticated(): boolean {
     return this.$store.getters.isCustomerAuthenticated
+  }
+
+  get hideColorSection() {
+    return this.$store.getters.getHideColorSection
   }
 
   public async initFirstLogoTab(index: number){
