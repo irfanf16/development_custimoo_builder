743c9c4b415e3c5338a6f23ca89085f003c0f0cb
gulzaryousafwnw <gy@weiswise.com>
2021-08-12 04:24:11 -0700

    UI changes

--- src/components/DesignCollectionModal.vue
+++ src/components/DesignCollectionModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-modal ref="collection-modal" id="modal-center-collection" scrollable size="xl" content-class="collection-modal">
+  <b-modal ref="collection-modal" id="modal-center-collection" size="xl" content-class="collection-modal">
     <template #modal-title>
       <div class="d-flex align-items-center justify-content-between w-100">
         <div>
@@ -17,7 +17,7 @@
 
     <template #modal-footer>
       <div class="d-flex align-items-center justify-content-end w-100 gap-1">
-        <b-button variant="secondary" class="light">Cancel</b-button>
+        <b-button @click="hideCollectionModal" variant="secondary" class="light">Cancel</b-button>
         <b-button variant="secondary">Save</b-button>
       </div>
     </template>
