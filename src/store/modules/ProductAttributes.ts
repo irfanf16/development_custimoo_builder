ee7ac93bf9391538480bb6780a964e1c81101a19
Mobeen <mb@weisewise.com>
2021-08-21 18:37:24 +0500

    disable checkbox functionality

--- src/store/modules/ProductAttributes.ts
+++ src/store/modules/ProductAttributes.ts
@@ -31,7 +31,7 @@ const ProductAttributes:Module<any, any> = {
     editProductId: 0,
     editDesignId: 0,
     editStyleId: 0,
-    selectedCollectionProducts: {locker_products:[],collection_id:0},
+    selectedCollectionProducts: {locker_products:[],disabled_products:[],collection_id:0},
     collections: [],
     designCollections: [],
     editProduct:{
@@ -63,6 +63,9 @@ const ProductAttributes:Module<any, any> = {
     },
     SET_ADD_MORE_COLLECTION(state: Record<any, any>, payload: boolean){
       state.addMoreCollection = payload
+    },
+    SET_DISABLED_PRODUCTS(state: Record<any, any>, payload: boolean){
+      state.selectedCollectionProducts.disabled_products = state.selectedCollectionProducts.locker_products
     },
     SET_PRODUCTS(state: Record<any, any>, payload: [Record<any, any>]){
       if(payload.length) {
@@ -486,6 +489,9 @@ const ProductAttributes:Module<any, any> = {
     },
     getSelectedCollectionProducts(state:Record<any, any>){
       return state.selectedCollectionProducts.locker_products
+    },
+    getDisabledProducts(state:Record<any, any>){
+      return state.selectedCollectionProducts.disabled_products
     },
     getSelectedCollectionParams(state:Record<any, any>){
       return state.selectedCollectionProducts
