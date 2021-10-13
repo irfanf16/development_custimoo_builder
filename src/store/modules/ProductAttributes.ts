5108a3fc32c13b01ccc08b9eb4ca4f33681fe29d
yasir <yr@weiswise.com>
2021-10-13 17:44:27 +0500

    logo move issue fixed

--- src/store/modules/ProductAttributes.ts
+++ src/store/modules/ProductAttributes.ts
@@ -198,8 +198,8 @@ const ProductAttributes:Module<any, any> = {
     },
     CUSTOM_LOGO_WITHOUT_TRIGGER(state: Record<any, any>, customLogoAttribute: Record<any, any>) {
       if(customLogoAttribute){
-        if(state.customLogos[customLogoAttribute.index]) {
-          Object.assign(state.customLogos[customLogoAttribute.index], customLogoAttribute.data)
+        if(state.customLogos[state.selectedPrdId] && state.customLogos[state.selectedPrdId][customLogoAttribute.index]) {
+          Object.assign(state.customLogos[state.selectedPrdId][customLogoAttribute.index], customLogoAttribute.data)
         }
       }
     },
