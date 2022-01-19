6cf9dadc9b842b9fd26ffd0dd0a9e2e2de56880b
shahzaib zulfiqar <hurroor@gmail.com>
2022-01-19 20:20:45 +0500

    added missing props

--- src/components/OrderDetails.vue
+++ src/components/OrderDetails.vue
@@ -129,6 +129,14 @@ export default class OrderDetails extends Mixins(ErrorMessages)  {
     return this.$store.getters.getSvgGroups
   }
 
+  get customLogos(): [Record<any, any>] {
+    return this.$store.getters.getCustomLogos().filter((custom_logo:any) => !(custom_logo == null || custom_logo.url == ""));
+  }
+
+  get customTexts(): [Record<any, any>] {
+    return this.$store.getters.getCustomTexts()
+  }
+
 
   get productionSVGs(): Record<any, any> {
     return this.$store.getters.getProductionSVGs
