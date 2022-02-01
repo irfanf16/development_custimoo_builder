edd8774a64601315d1e4a3a6af56647ef671ffe1
gulzaryousafwnw <gy@weiswise.com>
2022-02-01 21:26:29 +0500

    Side tabs toggle

--- src/components/CustomizationText.vue
+++ src/components/CustomizationText.vue
@@ -2,83 +2,94 @@
   <div class="customization-text-area">
     <div class="px-3 pt-3 p-lg-4">
       <h2 class="fw-bold mb-2 fz-18">Player {{ customTexts[customTextIndex].type | capitalize }} {{ customTexts[customTextIndex].side }}</h2>
-      <b-form-input
-        @click="isHidden = !isHidden"
-        class="mb-2 mr-sm-2 mb-sm-0"
-        placeholder="Type Here"
-        :value="customTexts[customTextIndex].text"
-        @input="updateTextField(customTextIndex, $event)"
-      ></b-form-input>
-      <h4 class="mt-3 mb-2 fz-16">Font Type</h4>
-      <div class="font-type-area">
-        <div class="type-block">
-          <b-form-select :value="customTexts[customTextIndex].fontFamily" @change="fontOptionChanged(customTextIndex, $event)" :options="fontOptions" ></b-form-select>
-        </div>
-        <div class="arc-block">
-          <b-form-select :value="customTexts[customTextIndex].side" @change="changeSide(customTextIndex, $event)" :options="['front', 'back']"></b-form-select>
-        </div>
+      <div class="d-flex">
+        <b-form-input
+          @click="isHidden = !isHidden"
+          class="mb-2 mr-sm-2 mb-sm-0"
+          placeholder="Type Here"
+          :value="customTexts[customTextIndex].text"
+          @input="updateTextField(customTextIndex, $event)"
+        ></b-form-input>
+        <button @click="setOpenIndex(customTextIndex)" class="d-flex align-items-center btn btn-secondary light">
+          <BIconPlus /> Customize
+        </button>
       </div>
-      <h4 class="mt-3 mb-2 fz-16">Select Color</h4>
-      <div class="text-color-holder" :class="{ active: customTexts[customTextIndex].selectColor }">
-<!--      <div class="text-color-holder active" >-->
-        <a @click="showColor('fill', customTextIndex)">
-          <div class="text-color-box">
-            <div class="color-circle"
-                 :style="{ background : customTexts[customTextIndex].fillColor? customTexts[customTextIndex].fillColor : ' url(' + colorImage + ') no-repeat 50% 50% / 12px' }"></div>
-            <strong>Fill Color</strong>
+
+      <template v-if="openIndex == customTextIndex">
+        <h4 class="mt-3 mb-2 fz-16">Font Type</h4>
+        <div class="font-type-area">
+          <div class="type-block">
+            <b-form-select :value="customTexts[customTextIndex].fontFamily" @change="fontOptionChanged(customTextIndex, $event)" :options="fontOptions" ></b-form-select>
           </div>
-        </a>
-        <a @click="showColor('outline', customTextIndex)" v-if="customTexts[customTextIndex].outlineEnabled &&  customTexts[customTextIndex].outLineWidth > 0">
-          <div class="text-color-box">
-             <div class="color-circle"
-                 :style="{ background : customTexts[customTextIndex].outLineColor? customTexts[customTextIndex].outLineColor : ' url(' + colorImage + ') no-repeat 50% 50% / 12px' }"></div>
-            <strong>Outline Color</strong>
+          <div class="arc-block">
+            <b-form-select :value="customTexts[customTextIndex].side" @change="changeSide(customTextIndex, $event)" :options="['front', 'back']"></b-form-select>
           </div>
-        </a>
-        <div class="color-holder">
-          <b-card-body style="padding: 0 !important;">
-            <b-nav class="d-flex flex-wrap align-items-center">
-              <b-nav-item class="mr-2" v-for="(colorType, index) in fontsColors" :key="index" @click="selectType(index)">{{ colorType.file_type }}</b-nav-item>
-            </b-nav>
-
-
-
-<!--            <div class="color-holder">-->
-<!--              <div class="">-->
-<!--                <div class="color-box" v-for="(color, index) in fontColor" @click="setColor(color)"
-                     :title="color.name" :style="{background: color.value}" :key="index">
-
-                </div>-->
-
-                <TextColorTabs ref="text-color-tab" @setColors="setColor" :productColors="productColors" :showSVGS="Boolean(showSVGs)"/>
-
-
-
-
-<!--              </div>-->
-
-
-
-
-
-<!--            </div>-->
-
-
-
-          </b-card-body>
-
-
         </div>
-      </div>
-      <div class="outline-slider-area pt-4">
-        <template v-if="customTexts[customTextIndex].outlineEnabled">
-          <div>
-            <label for="range-2 fz-16">Outline Width</label>
-            <b-form-input class="mt-2" id="range-2"  :value="customTexts[customTextIndex].outLineWidth" @change="outLineWidthValueChanged($event)" type="range" min="0" max="10" step="1"></b-form-input>
-            <div class="mt-2">Outline Size: {{ customTexts[customTextIndex].outLineWidth }}px</div>
+        <h4 class="mt-3 mb-2 fz-16">Select Color</h4>
+        <div class="text-color-holder" :class="{ active: customTexts[customTextIndex].selectColor }">
+  <!--      <div class="text-color-holder active" >-->
+          <a @click="showColor('fill', customTextIndex)">
+            <div class="text-color-box">
+              <div class="color-circle"
+                   :style="{ background : customTexts[customTextIndex].fillColor? customTexts[customTextIndex].fillColor : ' url(' + colorImage + ') no-repeat 50% 50% / 12px' }"></div>
+              <strong>Fill Color</strong>
+            </div>
+          </a>
+          <a @click="showColor('outline', customTextIndex)" v-if="customTexts[customTextIndex].outlineEnabled &&  customTexts[customTextIndex].outLineWidth > 0">
+            <div class="text-color-box">
+               <div class="color-circle"
+                   :style="{ background : customTexts[customTextIndex].outLineColor? customTexts[customTextIndex].outLineColor : ' url(' + colorImage + ') no-repeat 50% 50% / 12px' }"></div>
+              <strong>Outline Color</strong>
+            </div>
+          </a>
+          <div class="color-holder">
+            <b-card-body style="padding: 0 !important;">
+              <b-nav class="d-flex flex-wrap align-items-center">
+                <b-nav-item class="mr-2" v-for="(colorType, index) in fontsColors" :key="index" @click="selectType(index)">{{ colorType.file_type }}</b-nav-item>
+              </b-nav>
+
+
+
+  <!--            <div class="color-holder">-->
+  <!--              <div class="">-->
+  <!--                <div class="color-box" v-for="(color, index) in fontColor" @click="setColor(color)"
+                       :title="color.name" :style="{background: color.value}" :key="index">
+
+                  </div>-->
+
+                  <TextColorTabs ref="text-color-tab" @setColors="setColor" :productColors="productColors" :showSVGS="Boolean(showSVGs)"/>
+
+
+
+
+  <!--              </div>-->
+
+
+
+
+
+  <!--            </div>-->
+
+
+
+            </b-card-body>
+
+
           </div>
-        </template>
-      </div>
+        </div>
+        <div class="outline-slider-area pt-4">
+          <template v-if="customTexts[customTextIndex].outlineEnabled">
+            <div>
+              <label for="range-2 fz-16">Outline Width</label>
+              <b-form-input class="mt-2" id="range-2"  :value="customTexts[customTextIndex].outLineWidth" @change="outLineWidthValueChanged($event)" type="range" min="0" max="10" step="1"></b-form-input>
+              <div class="mt-2">Outline Size: {{ customTexts[customTextIndex].outLineWidth }}px</div>
+            </div>
+          </template>
+        </div>
+      </template>
+      <template v-else>
+
+      </template>
     </div>
   </div>
 </template>
@@ -115,6 +126,7 @@ export default class CustomizationText extends Vue {
   @Prop({required: true}) fontsColors!: any
   @Prop({required: true}) customTextIndex!: any
   @Prop({required: true}) fontOptions!: any
+  @Prop({required: true}) index!: any
   public selectedFont = null
   public colorImage = '/img/images/color-placeholder.png'
   public fontColorType!: string
@@ -124,9 +136,14 @@ export default class CustomizationText extends Vue {
   public outLineWidthValue = 0
   public productColors: any[] = []
   public showSVGs = false
+  public openIndex = -1
 
   get productNames() {
     return this.$store.getters.getSelectedProduct.productnames;
+  }
+
+  public setOpenIndex(index:any){
+    this.openIndex = index;
   }
 
   get customTexts(): [Record<any, any>] {
