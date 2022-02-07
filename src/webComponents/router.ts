c24c01b5fe37a6fedd90caf43caaa339d9ec08fe
Shahzaib Zulfiqar <hurroor@gmail.com>
2022-02-07 15:51:16 +0500

    working on web component

---
+++ src/webComponents/router.ts
@@ -0,0 +1,47 @@
+import Vue from 'vue'
+import VueRouter, { RouteConfig } from 'vue-router'
+import Home from '../views/Home.vue'
+import ShareRoster from '../views/ShareRoster.vue'
+import ConfirmOrder from '../views/ConfirmOrder.vue'
+import CollectionViewPDF from "@/views/CollectionViewPDF.vue";
+
+Vue.use(VueRouter)
+
+const routes: Array<RouteConfig> = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/confirm-order',
+    name: 'ConfirmOrder',
+    component: ConfirmOrder
+  },
+  {
+    path:'/share/:product/:name',
+    name: 'ShareUrl',
+    component: Home
+  },
+  {
+    path:'/collection/:collection_file_name/view',
+    name: 'CollectionUrl',
+    component: CollectionViewPDF
+  },
+  {
+    path:'/shareRoster/:urlstring',
+    name: 'ShareRoster',
+    component: ShareRoster
+  },
+  {
+    path:'/admin/login',
+    name:'LoginAsAdmin',
+    component: Home,
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
