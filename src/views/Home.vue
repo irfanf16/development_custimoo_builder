960eaa426dd10e857d5fe202ea6730e914aa5efb
yasir <yr@weiswise.com>
2022-02-14 14:07:01 +0500

    one issue fixed for eslint

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -207,7 +207,7 @@
 
 <script lang="ts">
 
-import {Component, Prop, Mixins, Vue, Watch} from 'vue-property-decorator'
+import {Component, Mixins, Vue, Watch} from 'vue-property-decorator'
 import ChooseColor from '@/components/ChooseColor.vue'
 import CustomizationPreview from '@/components/CustomizationPreview.vue'
 import ItemToCustomize from '@/components/ItemToCustomize.vue'
@@ -364,14 +364,13 @@ export default class Home extends Mixins(ErrorMessages, LockerProducts, handleMa
 
   private switchTabs (e:Record<any, any>){
     this.ref['custom-mobile-tabs'].hideOtherTab()
-    // eslint-disable-next-line @typescript-eslint/no-this-alias
-    let that = this;
+    let self = this;
     $(".sideNav li a").removeClass('active')
     // e.target.classes.push('active');
     e.currentTarget.classList.add('active');
     $(".sideNav li a").each(function (index){
       if($(this).hasClass('active')){
-        that.$store.dispatch('setActiveTab', index);
+        self.$store.dispatch('setActiveTab', index);
       }
     })
   }
