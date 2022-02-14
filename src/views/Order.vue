2968232f92145fbde51c64819582768538b7b475
gulzaryousafwnw <gy@weiswise.com>
2022-02-14 15:03:10 +0500

    Order page progress

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="page-wrapper m-lg-4" v-cloak>
-    <div class="fs-3 font-weight-bolder">Order # 12331</div>
-    <b-tabs content-class="mt-3">
-      <b-tab :key="i" v-for="(item, i) in 3">
-        <template #title>
-          Factory {{item}}
-        </template>
-      </b-tab>
-    </b-tabs>
+    <div class="order-wrapper">
+      <div class="fs-4 font-weight-bolder order-title p-2">Order # 12331</div>
+      <b-tabs content-class="mt-3">
+        <b-tab :key="i" v-for="(item, i) in 3">
+          <template #title>
+            Factory {{item}}
+          </template>
+
+          <div class="order-flow">
+            <div class="order-step active">
+              Order<br>Created
+            </div>
+            <div class="order-step">
+              Artwork<br>Approval
+            </div>
+            <div class="order-step">
+              Order<br>Created
+            </div>
+            <div class="order-step">
+              Order<br>Completed
+            </div>
+          </div>
+        </b-tab>
+      </b-tabs>
+    </div>
   </div>
 </template>
 
@@ -28,5 +45,12 @@ export default class Order extends Mixins() {
 </script>
 
 <style lang="scss" scoped>
+.order-wrapper{
+  padding: 10px;
+  background: #efefef;
 
+  .order-title{
+    text-align: left;
+  }
+}
 </style>
