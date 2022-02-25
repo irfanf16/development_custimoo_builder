b2803831e00df90ae93046b7884a32f30acfb6f3
Shahzaib Zulfiqar <hurroor@gmail.com>
2022-02-25 12:41:13 +0500

    scene null issue resolved

--- src/components/Scene.vue
+++ src/components/Scene.vue
@@ -264,6 +264,9 @@ export default class Scene extends Vue {
             self.backCanvas.remove(this.customLogoObjects[logo.logoIndex])
           }
           this.customLogoObjects[logo.logoIndex] = null
+          if(this.mainPreview) {
+            this.$store.commit("UPDATE_CUSTOM_LOGO_OBJECTS", {index: logo.logoIndex, data: null, scene: self});
+          }
           if(this.otherSideLogos[index]) {
             this.frontCanvas.remove(this.otherSideLogos[index])
             if (this.backCanvas) {
@@ -278,6 +281,9 @@ export default class Scene extends Vue {
               this.backCanvas.remove(this.customLogoObjects[index])
             }
             this.customLogoObjects[index] = null
+            if(this.mainPreview) {
+              this.$store.commit("UPDATE_CUSTOM_LOGO_OBJECTS", {index: index, data: null, scene: self});
+            }
             if(this.otherSideLogos[index]) {
               this.frontCanvas.remove(this.otherSideLogos[index])
               if (this.backCanvas) {
@@ -1708,6 +1714,9 @@ export default class Scene extends Vue {
             })
           }
           this.customLogoObjects[logoIndex as number] = img
+          if(this.mainPreview) {
+            this.$store.commit("UPDATE_CUSTOM_LOGO_OBJECTS", {index: logoIndex, data: img, scene: self});
+          }
         } else {
           this.logoObjects.push(img)
         }
