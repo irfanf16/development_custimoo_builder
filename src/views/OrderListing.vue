5a895b96b52e38e64e275b2244bd9a8652f37a15
faisal nadeem <fn@techscale.io>
2022-03-08 02:40:44 -0800

    quantity count

--- src/views/OrderListing.vue
+++ src/views/OrderListing.vue
@@ -61,7 +61,7 @@
               {{ order.created_at | orderDate }}
             </td>
             <td>
-              {{order.items_count}}
+              {{order.product_count}}
             </td>
             <td>
               <a :href="`${storage_url}${order.design_file}`" target="_blank" class="btn btn-secondary mx-2">PDF</a><router-link  :to="`order/${order.id}/detail`" class="btn btn-secondary mx-2">Details</router-link>
@@ -81,7 +81,8 @@
                         <td>{{ product.product_name }}</td>
                         <td class="image"><img :src="`${storage_url}${product.front_image}`" class="img-thumbnail img-fluid" style="width: 80px"></td>
                         <td class="image"><img :src="`${storage_url}${product.back_image}`" class="img-thumbnail img-fluid" style="width: 80px"></td>
-                        <td>{{ item.status | Status }}</td>
+                        <td>{{ product.roster_detail[0].quantity }}</td>
+                        <td>{{ product.status | Status }}</td>
                       </tr>
                     </template>
                   </table>
