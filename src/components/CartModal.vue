dd7b534505ee86b285be24ccf5c21ee26830c303
gulzaryousafwnw <gy@weiswise.com>
2022-05-02 17:55:23 +0500

    Cart modal ui fixed

--- src/components/CartModal.vue
+++ src/components/CartModal.vue
@@ -1,105 +1,112 @@
 <template>
   <modal :minWidth ="800"
-         :minHeight="600" :resizable="true"
+         :minHeight="600"
+         :scrollable="true"
          :adaptive="true" name="cart-modal" ref="cart-modal" id="cart-center-lockerroom" size="xl"  t
          itle="User Cart" modal-class="modal-fullscreen2"  content-class="lockerroom-modal"
          @closed="customer_reference_no=null" @before-open="getAddresses"
         >
-
-    <div class="loader relative" v-if="viewLoader"><img src="../../src/assets/images/loading.gif" /></div>
-    <table class="table table-bordered b-table-fixed mb-0 w-100" v-if="cartItems">
-      <thead class="bg-light">
-      <tr>
-        <th class="font-weight-bold">
-          Product Name
-        </th>
-        <th class="font-weight-bold">
-          Design Image
-        </th>
-        <th class="font-weight-bold">
-          Quantity
-        </th>
-        <th colspan="2" class="font-weight-bold">
-          Actions
-        </th>
-      </tr>
-      </thead>
-      <tbody>
-      <template v-for="(cart_item) in cartItems">
-        <tr :key="factory_product.id" v-for="(factory_product) in cart_item.factory_products">
-          <td>
-            {{factory_product.product_name}}
-          </td>
-          <td><b-img style="width: 80px" thumbnail fluid :src="storageUrl+factory_product.front_image" alt="Front Design"></b-img>
-            <b-img style="width: 80px" thumbnail fluid :src="storageUrl+factory_product.back_image" alt="Back Design"></b-img>
-
-          </td>
-          <td>
-            <template v-for="(roster_detail,index) in factory_product.roster_detail">
-              <div :key="index"><span>{{roster_detail.size}} : {{roster_detail.quantity}}</span></div>
-            </template>
-            <div>Total : {{factory_product.roster_detail | itemQtyCount(factory_product.roster_detail)}}</div>
-          </td>
-<!--          <td>{{factory_product.roster_detail | itemQtyCount(factory_product.roster_detail)}}</td>-->
-          <td class="cursor-pointer">   <a data-title="Edit Product" @click="editCartItem(factory_product,cart_item.id)">
-            <font-awesome-icon
-              :icon="['fas', 'edit']"/>
-          </a></td>
-          <td class="cursor-pointer">  <a data-title="Delete Event" @click="deleteConfirm(cart_item,factory_product)"
-                                        >
-            <font-awesome-icon
-              :icon="['fas', 'trash-alt']"/>
-          </a></td>
-        </tr>
-      </template>
-      </tbody>
-    </table>
-
-    <div class="p-3 grid grid-mobile-2 gap-2 text-left">
-      <div class="well border-0" style="background: #f5f5f5">
-        <div class="fs-2 font-weight-bold px-2 pt-1 pb-2">
-          Shipping Address
-        </div>
-        <template v-if="shipping_address">
-          <div class="px-2 pt-1 pb-2">
-            <div>{{shipping_address.first_name}} {{shipping_address.last_name}}</div>
-            <div>{{shipping_address.address1}}</div>
-            <div>{{shipping_address.address2}}</div>
-            <div>{{shipping_address.zip_code}}</div>
-            <div>{{shipping_address.country.name}} {{shipping_address.city}}</div>
-
-            <div class="d-flex flex-wrap w-100">
-              <div>{{shipping_address.phone_number}}</div>
-
-              <router-link :to="'address?cart=1'" class="btn ml-auto align-self-end btn-dark medium btn-sm my-orders">
-                <span style="font-size: 0.85em"><b-icon-pencil /></span> Change
-              </router-link>
-            </div>
-
-          </div>
-        </template>
-        <div v-else class="px-2 pt-1 pb-2">
-          <router-link :to="'address?cart=1'" class="btn btn-secondary btn-sm my-orders">
-            <span style="font-size: 0.9em"><b-icon-plus /></span> Add
-          </router-link>
-        </div>
-      </div>
-
-      <div class="align-self-end">
-        <div class="fs-2 font-weight-bold ">Reference No:</div>
-        <div class="mt-1">
-          <b-form-input   class="form-input" placeholder="Customer Reference No." type="text" name="customer_reference_no"
-                          v-model="customer_reference_no" />
-        </div>
-      </div>
+    <div class="modal-header d-flex justify-content-between">
+      <span class="fs-5 font-weight-bold">Cart</span>
+      <span class="fs-5 font-weight-bold cursor-pointer modal-close" @click="hideVModal('cart-modal')"><BIconX /></span>
     </div>
 
-    <template #modal-footer>
-      <div class="text-right">
-        <b-button   v-b-modal.modal-center-existingCollection variant="secondary" style="margin-right: 5px">Add to existing collection</b-button>
+    <div class="theme-scroll" style="height: calc(100% - 65px); overflow-y: auto; padding-bottom: 20px">
+      <div class="loader relative" v-if="viewLoader"><img src="../../src/assets/images/loading.gif" /></div>
+      <table class="table table-bordered b-table-fixed mb-0 w-100" v-if="cartItems">
+        <thead class="bg-light">
+        <tr>
+          <th class="font-weight-bold">
+            Product Name
+          </th>
+          <th class="font-weight-bold">
+            Design Image
+          </th>
+          <th class="font-weight-bold">
+            Quantity
+          </th>
+          <th colspan="2" class="font-weight-bold">
+            Actions
+          </th>
+        </tr>
+        </thead>
+        <tbody>
+        <template v-for="(cart_item) in cartItems">
+          <tr :key="factory_product.id" v-for="(factory_product) in cart_item.factory_products">
+            <td>
+              {{factory_product.product_name}}
+            </td>
+            <td><b-img style="width: 80px" thumbnail fluid :src="storageUrl+factory_product.front_image" alt="Front Design"></b-img>
+              <b-img style="width: 80px" thumbnail fluid :src="storageUrl+factory_product.back_image" alt="Back Design"></b-img>
+
+            </td>
+            <td>
+              <template v-for="(roster_detail,index) in factory_product.roster_detail">
+                <div :key="index"><span>{{roster_detail.size}} : {{roster_detail.quantity}}</span></div>
+              </template>
+              <div>Total : {{factory_product.roster_detail | itemQtyCount(factory_product.roster_detail)}}</div>
+            </td>
+            <!--          <td>{{factory_product.roster_detail | itemQtyCount(factory_product.roster_detail)}}</td>-->
+            <td class="cursor-pointer">   <a data-title="Edit Product" @click="editCartItem(factory_product,cart_item.id)">
+              <font-awesome-icon
+                :icon="['fas', 'edit']"/>
+            </a></td>
+            <td class="cursor-pointer">  <a data-title="Delete Event" @click="deleteConfirm(cart_item,factory_product)"
+            >
+              <font-awesome-icon
+                :icon="['fas', 'trash-alt']"/>
+            </a></td>
+          </tr>
+        </template>
+        </tbody>
+      </table>
+
+      <div class="p-3 grid grid-mobile-2 gap-2 text-left">
+        <div class="well border-0" style="background: #f5f5f5">
+          <div class="fs-2 font-weight-bold px-2 pt-1 pb-2">
+            Shipping Address
+          </div>
+          <template v-if="shipping_address">
+            <div class="px-2 pt-1 pb-2">
+              <div>{{shipping_address.first_name}} {{shipping_address.last_name}}</div>
+              <div>{{shipping_address.address1}}</div>
+              <div>{{shipping_address.address2}}</div>
+              <div>{{shipping_address.zip_code}}</div>
+              <div>{{shipping_address.country.name}} {{shipping_address.city}}</div>
+
+              <div class="d-flex flex-wrap w-100">
+                <div>{{shipping_address.phone_number}}</div>
+
+                <router-link :to="'address?cart=1'" class="btn ml-auto align-self-end btn-dark medium btn-sm my-orders">
+                  <span style="font-size: 0.85em"><b-icon-pencil /></span> Change
+                </router-link>
+              </div>
+
+            </div>
+          </template>
+          <div v-else class="px-2 pt-1 pb-2">
+            <router-link :to="'address?cart=1'" class="btn btn-secondary btn-sm my-orders">
+              <span style="font-size: 0.9em"><b-icon-plus /></span> Add
+            </router-link>
+          </div>
+        </div>
+
+        <div class="align-self-end">
+          <div class="fs-2 font-weight-bold ">Reference No:</div>
+          <div class="mt-1">
+            <b-form-input   class="form-input" placeholder="Customer Reference No." type="text" name="customer_reference_no"
+                            v-model="customer_reference_no" />
+          </div>
+        </div>
       </div>
-    </template>
-    <b-button class="mt-4" @click="createOrder">Finalize Order</b-button>
+
+      <!--    <template #modal-footer>-->
+      <!--      <div class="text-right">-->
+      <!--        <b-button   v-b-modal.modal-center-existingCollection variant="secondary" style="margin-right: 5px">Add to existing collection</b-button>-->
+      <!--      </div>-->
+      <!--    </template>-->
+      <b-button class="mt-4" @click="createOrder">Finalize Order</b-button>
+    </div>
   </modal>
 
 </template>
