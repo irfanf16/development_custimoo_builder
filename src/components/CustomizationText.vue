b233d28433dd339dde5ecba63c9edb145a686d18
ramzan1122 <ra@weiswise.com>
2022-06-01 10:42:59 +0500

    add new option team name

--- src/components/CustomizationText.vue
+++ src/components/CustomizationText.vue
@@ -12,7 +12,8 @@
         </template>
       </h2>
       <h2 class="fw-bold mb-2 fz-18 d-flex align-items-center justify-content-between" v-else>
-        <span>Player {{ customTexts[customTextIndex].type | capitalize }}</span>
+        <span v-if="customTexts[customTextIndex].type == 'text'">Team Name  {{ customTexts[customTextIndex].side }}</span>
+        <span v-else>Player {{ customTexts[customTextIndex].type | capitalize }} {{ customTexts[customTextIndex].side }}</span>
         <template v-if="customTextIndex + 1 > selectedProduct.productnames.length">
           <b-button class="ml-1 light" style="min-width: unset; line-height: normal" variant="dark"
             @click="$emit('removeTab')">
