61b3bcd56e0c580036a372ce5cf4d3c76a868cf2
ramzan1122 <ra@weiswise.com>
2022-06-01 11:26:25 +0500

    add new option team name

--- src/components/CustomizationText.vue
+++ src/components/CustomizationText.vue
@@ -12,7 +12,7 @@
         </template>
       </h2>
       <h2 class="fw-bold mb-2 fz-18 d-flex align-items-center justify-content-between" v-else>
-        <span v-if="customTexts[customTextIndex].type == 'text'">Team Name  {{ customTexts[customTextIndex].side }}</span>
+        <span v-if="customTexts[customTextIndex].type == 'text'">Team Name </span>
         <span v-else>Player {{ customTexts[customTextIndex].type | capitalize }} {{ customTexts[customTextIndex].side }}</span>
         <template v-if="customTextIndex + 1 > selectedProduct.productnames.length">
           <b-button class="ml-1 light" style="min-width: unset; line-height: normal" variant="dark"
