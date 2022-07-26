de08cb20551f15bb319cc4ae19c64df88542d615
yasirrasool <yr@weiswise.com>
2022-07-26 12:13:06 +0500

    value change to 0 for typescript-eslint/ban-ts-comment in eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,6 @@ module.exports = {
     "no-explicit-any": 0,
     "no-var": 0,
     "no-mixed-spaces-and-tabs": 0,
-    "@typescript-eslint/ban-ts-comment": true
+    "@typescript-eslint/ban-ts-comment": 0
   }
 }
