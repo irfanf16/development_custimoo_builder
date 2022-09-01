c8ae39c906e8858beb86c0473aa861f1a9847fbe
gulzaryousafwnw <gy@weiswise.com>
2022-09-01 15:04:06 +0500

    added categories for mobile

--- src/components/ItemToCustomize.vue
+++ src/components/ItemToCustomize.vue
@@ -7,6 +7,19 @@
       </h2>
 
       <div class="select-items" :class="[showItems ? 'opened' : '']">
+        <div class="d-flex align-items-center" v-if="getProductEditInfoObject.editing==false || (getProductEditInfoObject.editing===true && getProductEditInfoObject.locker_product_info !== null) ">
+          <div class="fade-right w-100 py-2" >
+            <div class="overflow-auto d-flex align-items-center theme-scroll-h pb-2 pointer gap-2 brandsList ">
+              <div  v-for="(category, categoryIndex) in categories" :key="`category_${categoryIndex}`" style="white-space: nowrap"
+                    :style="{color: (selectedCategory.index == categoryIndex) ? '#000 !important': '#999 !important'}"
+                    :class="{ 'pr-3': categoryIndex + 1 == categories.length, 'activeBrand': (selectedCategory.index == categoryIndex) }"
+                    role="button" @click="handleCategoryUpdate(categoryIndex)">
+                  <img :src="`${storage_url}${category.image_url}`"  height="30">
+                </div>
+            </div>
+          </div>
+        </div>
+
 <!--        <div class="collection-btn mt-1 px-1 d-flex align-items-center checkbox_buttons gap-2" v-if="StockCount > 0">-->
         <div class="collection-btn mt-1 px-1 d-flex align-items-center checkbox_buttons gap-2" v-if="StockCount > 0">
 <!--          <b-form-checkbox :checked="customized" @change="changeProductType('customized')"  class="mr-3" name="check-button" button key="Customized"><span class="checked"><b-icon icon="check-circle-fill"></b-icon></span> Customized</b-form-checkbox>-->
@@ -61,7 +74,6 @@
            </b-input-group>
          </div>
        </template>
-
       </div>
 
       <div class="d-flex align-items-center" v-if="getProductEditInfoObject.editing==false || (getProductEditInfoObject.editing===true && getProductEditInfoObject.locker_product_info !== null) ">
@@ -71,7 +83,7 @@
                   :style="{color: (selectedCategory.index == categoryIndex) ? '#000 !important': '#999 !important'}"
                   :class="{ 'pr-3': categoryIndex + 1 == categories.length, 'activeBrand': (selectedCategory.index == categoryIndex) }"
                   role="button" @click="handleCategoryUpdate(categoryIndex)">
-              <img :src="`${storage_url}${category.image_url}`"  height="30">
+                <img :src="`${storage_url}${category.image_url}`"  height="30">
               </div>
           </div>
         </div>
