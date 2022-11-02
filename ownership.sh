ab326da72628746a985ba84c58848478641803ff
Nisar Ahmed <nisar.devops@gmail.com>
2022-11-02 15:14:03 +0500

    setup autodeployment

---
+++ ownership.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+chown -R kitbuilder-autodeployment:kitbuilder-autodeployment /home/kitbuilder-autodeployment/public_html/
---
+++ permissions.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+#chmod 755 /home/kitbuilder-autodeployment/public_html
