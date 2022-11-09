47fad70ddf0cba97a5f2264b01cee4c42779883b
Nisar Ahmed <nisar.devops@gmail.com>
2022-11-09 14:08:30 +0500

    copy build files

--- ownership.sh
+++ ownership.sh
@@ -5,8 +5,8 @@ chown -R kitbuilder-autodeployment:kitbuilder-autodeployment /home/kitbuilder-au
 cd /home/kitbuilder-autodeployment/public_html/ && runuser -u kitbuilder-autodeployment npm run build:wc:customizer:self
 cd /home/kitbuilder-autodeployment/public_html/ && runuser -u kitbuilder-autodeployment npm run build:wc:customizer:wordpress
 cd /home/kitbuilder-autodeployment/public_html/ && runuser -u kitbuilder-autodeployment npm run build:wc:orderdetail
-mv /home/kitbuilder-autodeployment/public_html/self_customizer/* /home/cdncustimoo/public_html/self_staging/
-mv /home/kitbuilder-autodeployment/public_html/wordpress_customizer/* /home/cdncustimoo/public_html/wordpress_staging/
-mv /home/kitbuilder-autodeployment/public_html/order_detail_customizer/* /home/cdncustimoo/public_html/santa_order_detail_staging/  
+cp -R /home/kitbuilder-autodeployment/public_html/self_customizer/* /home/cdncustimoo/public_html/self_staging/
+cp -R /home/kitbuilder-autodeployment/public_html/wordpress_customizer/* /home/cdncustimoo/public_html/wordpress_staging/
+cp -R /home/kitbuilder-autodeployment/public_html/order_detail_customizer/* /home/cdncustimoo/public_html/santa_order_detail_staging/  
 chown -R cdncustimoo:cdncustimoo /home/cdncustimoo/public_html
 
