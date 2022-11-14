dba70ecd5ad2a73b294a51d3df7840268271fddf
gulzaryousafwnw <gy@weiswise.com>
2022-11-14 18:30:14 +0500

    Is not vector logo

--- src/components/LogoPlacementTabs.vue
+++ src/components/LogoPlacementTabs.vue
@@ -13,7 +13,7 @@
       <b-tab v-for="(logo_tab, ltIdx) in customLogos" :key="ltIdx" :active="tabIndex === ltIdx" @click="changeTab(ltIdx)">
         <template #title>
           <span>
-            <span v-if="!logo_tab.url" class="warning" :class="{'error': !vector_logos && logo_tab.url}">
+            <span class="warning" :class="{'error': !vector_logos && logo_tab.url}">
               <span v-b-tooltip.bottom="`Logo uploaded are not in vector format, please reupload to place order`" v-if="!vector_logos && logo_tab.url"><b-icon-exclamation-circle-fill /></span>
               <b-icon-exclamation-triangle-fill v-else />
             </span>
