53e8f11f272173e32d1fc7b5d04dee5c2bc34629
Shahzaib Zulfiqar <shahzaib.dev@gmail.com>
2022-12-09 16:09:17 +0500

    santa modal plugin finalized

--- src/main.ts
+++ src/main.ts
@@ -103,6 +103,9 @@ Vue.prototype.$logData = logData;
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
+import SantaModal from "@/plugins/santaModal/SantaModal.js";
+Vue.use(SantaModal)
+
 Vue.use(VueSweetalert2);
 new Vue({
   router,
