5879da91c229be96d7d5a82bba98c93e3afc5a5b
Johnny Serup <js@custimoo.com>
2023-03-13 13:31:27 +0000

    Merged PR 17: Added setup_ubuntu2004_custimoo_builder_environment.sh
    Added setup_ubuntu2004_custimoo_builder_environment.sh
    
    Related work items: #35

---
+++ setup_ubuntu2004_custimoo_builder_environment.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+PARENT_DIR="${PWD##*/}"
+
+apt-get update
+
+### Install curl
+apt-get install curl
+### Install NVM
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+command -v nvm
+
+# Source the NVM script to load it into the current shell
+source ~/.nvm/nvm.sh
+
+nvm --version 
+nvm install v14.16
+nvm use v14.16  ## using node v14.16.1 (npm v6.14.12)
+npm install -g svgo
+npm cache clean --force
+npm install
+npm install chromedriver --chromedriver_version=93.0.4577.63
+### Compiles and minifies for production
+npm run build
