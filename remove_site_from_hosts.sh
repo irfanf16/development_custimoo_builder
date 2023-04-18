013cdac71b53923e17345dedf4758f9a6c6cfed0
Johnny Serup <js@custimoo.com>
2023-04-18 14:21:45 +0000

    Merged PR 65: add missing dependend file
    add missing dependend file;
    
     #217
    
    Related work items: #217

---
+++ remove_site_from_hosts.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#line_to_remove="custimoo-v22-backend.test"
+line_to_remove=$1
+
+# Check if the line exists in the file
+if sudo grep -qF "$line_to_remove" /mnt/c/Windows/System32/drivers/etc/hosts; then
+    # Remove the line using sed
+
+sudo tee /mnt/c/Windows/System32/drivers/etc/hosts >/dev/null <<EOF
+$(sudo cat /mnt/c/Windows/System32/drivers/etc/hosts | sudo sed "/$line_to_remove/d")
+EOF
+    
+    echo "Line removed from hosts file."
+else
+    echo "Line not found in hosts file."
+fi
+
+# works
+#sudo tee /mnt/c/Windows/System32/drivers/etc/hosts >/dev/null <<EOF
+#$(sudo cat /mnt/c/Windows/System32/drivers/etc/hosts | sudo sed "/$line_to_remove/d")
+#EOF
+
