f064c5777386caec8060f46e4fbe1a44344290cf
Gulzar Yousaf <gy@custimoo.com>
2023-11-21 04:23:24 +0000

    Merged PR 1027: Merged PR 1026: Scroll issue fixed for the cart modal to access the confirm o...
    Merged PR 1026: Scroll issue fixed for the cart modal to access the confirm order button #1306
    
    Scroll issue fixed for the cart modal to access the confirm order button #1306
    
    ![image.png](https://dev.azure.com/Custimoo/f28161ec-6c1b-486c-a623-ae20c4419714/_apis/git/repositories/60fa9341-c503-4982-94ca-4516dc4925a5/pullRequests/1026/attachments/image.png)
    
    Related work items: #1306
    
    Related work items: #1253, #1306

--- src/components/CartModal.vue
+++ src/components/CartModal.vue
@@ -1,5 +1,5 @@
 <template>
-  <modal :minWidth="1000" :scrollable="true" :height="'auto'" :minHeight="600" :resizable="true" :adaptive="true"
+  <modal :minWidth="1000" :scrollable="true" :minHeight="600" :resizable="true" :adaptive="true"
     name="cart-modal" ref="cart-modal" id="cart-center-lockerroom" size="xl" class="cart-modal" modal-class="modal-fullscreen2"
     content-class="lockerroom-modal" @closed="customer_reference_no = null" @before-open="getAddresses">
     <div class="modal-header d-flex justify-content-between">
