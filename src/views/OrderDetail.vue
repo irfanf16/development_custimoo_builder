53b56dd22c11273c7c9f6eab27fa6eb24b5b6744
Muhammad Ramzan <mr@custimoo.com>
2025-07-02 07:19:34 +0000

    Merged PR 5178: Ecommerce: Redirect users to the login page if they are not logged in and try...
    Ecommerce: Redirect users to the login page if they are not logged in and try to access the order detail page. #4546;
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai -->
    
    ## Summary by CodeRabbit
    
    * **New Features**
      * Improved authentication handling for ecommerce platform users. Unauthenticated users are now either redirected to a specified URL or a custom login action is executed, depending on company settings.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    Related work items: #4546

--- src/views/OrderDetail.vue
+++ src/views/OrderDetail.vue
@@ -713,6 +713,14 @@ import { isValidEmail } from "@/helpers/Helpers";
         }
       });
 
+    }else{
+      if(isEcommercePlatform()){
+          if (this.company.login_code.type == 'url') {
+            window.location.href = this.company.login_code.action
+          } else {
+            eval(this.company.login_code.action)
+          }
+      }
     }
 
   },
