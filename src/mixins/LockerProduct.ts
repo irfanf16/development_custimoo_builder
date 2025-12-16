0839b638aa93e1b6f3a811d66582e2d63104aa1c
Shahzaib Zulfiqar <sz@custimoo.com>
2025-12-16 09:16:44 +0000

    Merged PR 8093: console removed
    Related work items: #5572

--- src/mixins/LockerProduct.ts
+++ src/mixins/LockerProduct.ts
@@ -2264,9 +2264,7 @@ export class CollectionMixin extends Mixins(ErrorMessages) {
 export class CustomerShopMixin extends Mixins() {
 
   mounted() {
-    if((this as any).$can('create-shop')){
-      this.getDefaultCoverPhotos();
-    }
+    this.getDefaultCoverPhotos();
   }
   public customerShopsDefaultCoverPhotosUrls = [];
   public async getDefaultCoverPhotos() {
